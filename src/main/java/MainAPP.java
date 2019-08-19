faa071f097aac64b935da2c47c810b46e1cc7b81
Artek <a.kowalski@ariesrk.pl>
2019-08-19 11:57:36 +0200

    attribution file to List

--- src/main/java/MainAPP.java
+++ src/main/java/MainAPP.java
@@ -1,18 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainAPP {
     public static void main(String[] args) throws FileNotFoundException {
 
+        Products products;
         File file = new File("/home/whysoserious/Desktop/Products/products.txt");
-
         Scanner scanner = new Scanner(file);
 
+        List<String> productsList = new ArrayList<>();
         while(scanner.hasNext()) {
             String line = scanner.nextLine();
-            System.out.println(line);
+            productsList.add(line);
         }
-
+        System.out.println(productsList);
     }
 }
