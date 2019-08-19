3ba39b99c3d57d02204a3459afd3f0d539e9e448
Artek <a.kowalski@ariesrk.pl>
2019-08-19 11:33:33 +0200

    add file

--- src/main/java/MainAPP.java
+++ src/main/java/MainAPP.java
@@ -1,6 +1,18 @@
-public class MainAPP {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-        System.out.println("Start");
+public class MainAPP {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("/home/whysoserious/Desktop/Products/products.txt");
+
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+
     }
 }
